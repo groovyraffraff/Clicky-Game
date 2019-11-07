28c838016a8ac340f6bb614379c1ce772e30e01e
Rafael <rafael.a.samano@gmail.com>
2019-11-07 14:38:13 -0600

    Updates

---
+++ precache-manifest.0c10c0731f7c83f4def942cf0445105f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9f7e6d936e417a0f50ba7239f12c19",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "875cca3ac841fe99ff95",
+    "url": "/Clicky-Game/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4dc0ed121523256a93b0",
+    "url": "/Clicky-Game/static/css/main.313a33d9.chunk.css"
+  },
+  {
+    "revision": "875cca3ac841fe99ff95",
+    "url": "/Clicky-Game/static/js/2.2d6582d7.chunk.js"
+  },
+  {
+    "revision": "4dc0ed121523256a93b0",
+    "url": "/Clicky-Game/static/js/main.40d6e9a6.chunk.js"
+  },
+  {
+    "revision": "75fa83e2a97bc1c69d72",
+    "url": "/Clicky-Game/static/js/runtime~main.4267543b.js"
+  }
+]);
